7b680f77a66a208e94b1d7bbe4ac2b3ba2cd3c62
Vilic Vane <i@vilic.info>
2019-05-21 07:44:02 +0000

    Update

--- scripts/build-dependencies.js
+++ scripts/build-dependencies.js
@@ -1,8 +1,38 @@
 #!/usr/bin/env node
 
-const fetch = require('fetch');
+const fetch = require('node-fetch');
 const {main} = require('main-function');
+const v = require('villa');
 
-main(async ([TOKEN]) => {
-  console.log(process.env);
+const {CI_API_V4_URL, CI_JOB_TOKEN} = process.env;
+
+const PROJECTS = [
+  {
+    id: 21,
+    branch: 'master',
+  },
+];
+
+main(async () => {
+  await v.parallel(PROJECTS, project => buildProject(project));
 });
+
+async function buildProject({id, branch}) {
+  await trigger();
+
+  async function trigger() {
+    let response = await fetch({
+      method: 'POST',
+      url: `${CI_API_V4_URL}/projects/${id}/pipeline?ref=${branch}`,
+      headers: {
+        'PRIVATE-TOKEN': CI_JOB_TOKEN,
+      },
+    });
+
+    console.log(response.status);
+
+    let json = await response.text();
+
+    console.log(json);
+  }
+}
